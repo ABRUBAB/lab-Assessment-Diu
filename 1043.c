3af0998f52bcfa7f7b8d3874b124008d5c81f901
Abdullah Rubab <100474368+ABRUBAB@users.noreply.github.com>
2023-04-05 21:53:01 +0600

    Create 1043.c

---
+++ 1043.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,c;
+	scanf("%f %f %f",&a,&b,&c);
+
+	if(a+b>c&&b+c>a&&a+c>b)
+		{
+	      printf("Perimetro = %0.1f\n",a+b+c);
+        }
+  else
+    	{
+    		printf("Area = %0.1f\n",(a+b)*(c/2));
+    		
+    	}
+  return 0;
+}
