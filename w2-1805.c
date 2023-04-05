3f239dd9c41a9c4fce1655919a5b756e8433bbbc
Abdullah Rubab <100474368+ABRUBAB@users.noreply.github.com>
2023-04-05 21:37:04 +0600

    Create w2-1805.c

---
+++ w2-1805.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   long long int i= pow(10,9);
+   long long int A,B,x,y,sum;
+   scanf("%lld %lld",&A,&B);
+   if(1 <= A && B<=i)
+      {
+   	     x = (B*(B+1))/2;
+	       y = ((A-1)*A)/2;
+   	     sum = x-y;
+         printf("%lld\n",sum);
+       }
+   return 0;
+}
