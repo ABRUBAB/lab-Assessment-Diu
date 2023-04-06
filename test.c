1a8f6a812359f08f14940deff226fc9e5bbbba95
Abdullah Rubab <100474368+ABRUBAB@users.noreply.github.com>
2023-04-06 14:40:30 +0600

    Create test.c

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+ float A;
+ scanf("%.2f",&A);
+ if(A<=400 && A>=0)
+    
+ else if(A<=800 && A>=400.01)
+ else if(A<=1200 && A>=800.01)
+ else if(A<=2000 && A>=1200.01)
+ else if(A>2000)
+ 
+ return 0;
+}
